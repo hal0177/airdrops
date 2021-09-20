9839c78b7392a7d96f14458a8159b2881e02fc18
Paddy Curé <paddy.cure@gmail.com>
2021-09-20 16:56:24 +0200

    sending

--- scripts/2_referrers_fsn.js
+++ scripts/2_referrers_fsn.js
@@ -28,7 +28,7 @@ const connect = async () => {
 }
 
 
-const startRewarding = async () => {  
+const startRewarding = async () => {
   let current = 0
   let finalIndex = allAddresses.length - 1
 
@@ -43,29 +43,28 @@ const startRewarding = async () => {
     let batchSize = (currentBatchEnd - currentBatchStart) + 1
     current = currentBatchEnd + 1
 
-    const results = fs.readFileSync("./results/referrers-fsn-results.json")
-    const { SUCCESS, FAIL } = JSON.parse(results)
+    const sendResults = fs.readFileSync("./results/referrers-fsn-results.json")
+    const { SUCCESS, FAIL } = JSON.parse(sendResults)
 
     console.log(`Connecting ...`)
     const { web3, sender } = await connect()
-    console.log(`Connected.`)
+    console.log(`Connected, ${ sender }`)
 
     console.log(`Airdropping batch ${ currentBatchStart } - ${ currentBatchEnd } / ${ finalIndex }`)
 
     let fsnRequests = []
 
     for(let i = 0; i < batchSize; i++) {
-      console.log(sender)
       let beneficiary = addresses[i]
-      // fsnRequests.push(web3.eth.sendTransaction({
-      //   from: sender,
-      //   to: beneficiary,
-      //   value: web3.utils.toWei(FSN_REWARD, "ether"),
-      //   gasLimit: "0x5208",
-      //   gasPrice: "0xb2d05e00",
-      //   nonce: txCount + i + 1
-      // }))
-      fsnRequests.push(`this is a promise ${ beneficiary }`)
+      console.log(`${ txCount + i + 1}: ${ beneficiary }`)
+      fsnRequests.push(web3.eth.sendTransaction({
+        from: sender,
+        to: beneficiary,
+        value: web3.utils.toWei(FSN_REWARD, "ether"),
+        gas: 21000,
+        gasPrice: 3000000000,
+        nonce: txCount + i + 1
+      }))
     }
 
     addresses = allAddresses.slice(currentBatchEnd + 1, currentBatchEnd + 1 + batchSize)
@@ -73,8 +72,8 @@ const startRewarding = async () => {
     
     const fsnResults = await Promise.allSettled(fsnRequests)
 
-    const fsnSuccesses = fsnResults.filter(res => res.status === "fulfilled").map(res => res.to)
-    const fsnFails = fsnResults.filter(res => res.status === "rejected").map(res => res.to)
+    const fsnSuccesses = fsnResults.filter(res => res.status === "fulfilled")
+    const fsnFails = fsnResults.filter(res => res.status === "rejected")
 
     const addResults = { SUCCESS: SUCCESS.concat(fsnSuccesses.map(res => res.to)), FAIL: FAIL.concat(fsnFails.map(res => res.to)) }
     const results = JSON.stringify(addResults, null, 2)
